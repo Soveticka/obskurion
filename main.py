eab626ef4c20a38f88d33ec66e268ce83a245d08
Soveticka <matejkomanek@outlook.cz@>
2025-03-29 14:52:31 +0100

    some changes

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class MyClient(discord.client):
     async def on_ready(self):
         print(f'Logged on as {self.user}')
 
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+        if message.author == client.user:
+            return
+        
+        if message.content.startswith('!hello'):
+            print(f'Message from {message.author}: {message.content}')
+            await message.channel.send('hello!')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run('token')
+client.run(os.getenv('BOT_TOKEN'))
